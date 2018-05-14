3f8f1c88eb082a074a659337ca66157dea7bead1
tohanian <tadehohanian@gmail.com>
2018-05-14 18:02:54 -0400

    update header styling

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -1,29 +1,30 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 // Components
-import About from "./About";
-import HomeSearch from "../components/HomeSearch";
-import HowItWorks from "../components/HowItWorks";
-import WhyChooseUs from "../components/WhyChooseUs";
-import CallToActionForm from "../components/CallToActionForm";
+import About from './About';
+import HomeSearch from '../components/HomeSearch';
+import HowItWorks from '../components/HowItWorks';
+import WhyChooseUs from '../components/WhyChooseUs';
+import CallToActionForm from '../components/CallToActionForm';
+import Header from './Header';
 // import ContactUs from "../components/ContactUs";
 // import Footer from "../components/Footer";
 
 class LandingPage extends Component {
-	render() {
-		return (
-			<div>
-				{/* <Header /> */}
-				<HomeSearch />
-				<HowItWorks />
-				<WhyChooseUs />
-				<CallToActionForm />
-				{/* <About />
+  render() {
+    return (
+      <div>
+        <Header />
+        <HomeSearch />
+        <HowItWorks />
+        <WhyChooseUs />
+        <CallToActionForm />
+        {/* <About />
 				<ContactUs />
 				<Footer /> */}
-			</div>
-		);
-	}
+      </div>
+    );
+  }
 }
 
 export default LandingPage;
--- src/containers/header.js
+++ src/containers/header.js
@@ -1,20 +1,24 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
-	render() {
-		return (
-			<nav>
-				<Link to="/">
-					<h1>Global Commerce</h1>
-				</Link>
-				<ul>
-					<li>Property Search</li>
-					<li>About</li>
-				</ul>
-			</nav>
-		);
-	}
+  render() {
+    return (
+      <nav>
+        <div className="logo-container">
+          <Link to="/">
+            <h1>Global Commerce</h1>
+          </Link>
+        </div>
+        <div className="nav-menu-container">
+          <ul className="nav-menu">
+            <li>Property Search</li>
+            <li>About</li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Header;
